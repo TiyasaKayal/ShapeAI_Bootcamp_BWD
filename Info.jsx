74840376dbc92f6d4a8e5aeb6c75c96cbbd96173
TiyasaKayal <86588130+TiyasaKayal@users.noreply.github.com>
2021-06-28 11:24:12 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div>
+      <h1>Javascript and react.js</h1>
+    <p>
+     A basic web dev React Js Bootcamp</p>
+    
+    </div>
+  );
+}
+
+export default Info;
+
